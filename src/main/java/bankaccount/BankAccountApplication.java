5fab5384589e8089f6dc89a34a3e9125846c076c
silva <silva@192.168.1.2>
2017-12-07 16:53:22 +0100

    Account je sada child od Client, filter statusa u transaction_list

--- src/main/java/bankaccount/BankAccountApplication.java
+++ src/main/java/bankaccount/BankAccountApplication.java
@@ -9,8 +9,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 import bankaccount.model.Account;
+import bankaccount.model.Client;
 import bankaccount.model.Transaction;
 import bankaccount.repository.AccountRepository;
+import bankaccount.repository.ClientRepository;
 import bankaccount.repository.TransactionRepository;
 import bankaccount.web.NewClientController;
 
@@ -24,24 +26,21 @@ public class BankAccountApplication {
 	}
 
 	@Bean
-	public CommandLineRunner demo(AccountRepository repository) {
+	public CommandLineRunner demo(ClientRepository repository) {
 		return (args) -> { 
-			// save a couple of customers
-			//Account prvi = new Account();	
-			/*
-			Account a1 = new Account("Jack", "Bauer", 12345);
-			Account a3 = new Account("Jacsrfsk", "Bauggger", 127673);
-			Account a5 = new Account("Jacjgjgk", "Baugggjer", 12453);
-		    //repository.save(prvi);
-			repository.save(a1);
-			repository.save(a3);
-			repository.save(a5);
-		
-			log.info("a1,a2,a3,a4,a5" + a1+ a3 + a5);
-			for (Account account : repository.findAll()) {
-				log.info("svi racuni _______________  " + account.toString());
+			//save a couple of customers
+			
+			
+			Client client = new Client("Jana", "1234");
+	
+			repository.save(client);
+	
+
+			log.info("client" + client);
+			for (Client c : repository.findAll()) {
+				log.info("svi racuni _______________  " + c.toString());
 			} 
-		*/
+		
 	
 		};
 	}
